00c75203a0add487ac691903ed2f634b8c2f9780
Matt Thompson <chatrat12@yahoo.com>
2015-10-16 10:48:34 -0700

    Added Autosaver for Build Mode

---
+++ Assets/Scripts/BoardLayout/AutoSaver.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+
+public class AutoSaver : MonoBehaviour
+{
+
+    public int SaveInterval = 60;
+    private float _lastSave = 0;
+
+    void Update()
+    {
+        if (Time.time - _lastSave >= SaveInterval)
+        {
+            Debug.Log("Autosaving");
+            SaveLevel(NewBoardLayout.FromGameboard(), "Autosave");
+            _lastSave = Time.time;
+        }
+    }
+
+
+    public static void SaveLevel(NewBoardLayout layout, string defaultName)
+    {
+        var path = "Assets/BoardLayouts/Autosave.asset";
+
+        AssetDatabase.CreateAsset(layout, path);
+        AssetDatabase.Refresh();
+        Debug.Log("Layout Saved");
+
+    }
+
+}
